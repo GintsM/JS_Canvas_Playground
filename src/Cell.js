69a9cf60c5bf19089537bfbcc6432b4cae7e23fe
Gints Misins <88575586+GintsM@users.noreply.github.com>
2022-09-27 23:27:32 +0300

    Enter number in grid -fn

--- src/Cell.js
+++ src/Cell.js
@@ -28,6 +28,31 @@ class Cell {// TODO create checkByLength() maybe
     }
     return this.value
   }
+
+  cellsOptions() {
+    let arr = []
+    arr = this.line.concat(this.col, this.grid)
+    console.log(arr, "cellsOptions")
+  }
 }
 
 export default Cell
+
+export function drawBox(ctx, ROW, boxSize, DIM) {
+  ctx.strokeStyle = 'black';
+  let x = 0, y = 0;
+  for (let row = 0; row <= ROW; row++) {
+    ctx.beginPath();
+    // X axe
+    x = row * boxSize;
+    (row % Math.sqrt(ROW)) === 0 ? ctx.lineWidth = 4 : ctx.lineWidth = 2;
+    ctx.moveTo(x, y)
+    ctx.lineTo(x, y + DIM)
+    ctx.stroke();
+    // Y axe
+    ctx.moveTo(y, x)
+    ctx.lineTo(y + DIM, x)
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
--- src/canvas.js
+++ src/canvas.js
@@ -2,56 +2,87 @@
 import getRandom from './ran_fr_arr.js'
 import Cell from './Cell.js';
 import { Table } from './Cell.js';
+import { drawBox } from './Cell.js';
 
 const canvas = document.getElementById("myCanvas");
+const DIM = 600;
+let ROW = 9
+let boxSize = Math.floor(DIM / ROW);
+const sqR = Math.sqrt(ROW)
+const roundedHeightWidth = DIM % ROW === 0 ? DIM : Math.floor(DIM / ROW) * ROW
 
-const canvasScreen = (ROWS = 1) => {
-  const DIM = 600;
-  const sqR = Math.sqrt(ROWS)
-  const roundedHeightWidth = DIM % ROWS === 0 ? DIM : Math.floor(DIM / ROWS) * ROWS
+canvas.setAttribute('width', `${roundedHeightWidth}px`)
+canvas.setAttribute('height', `${roundedHeightWidth}px`)
+const ctx = canvas.getContext("2d");
 
-  canvas.setAttribute('width', `${roundedHeightWidth}px`)
-  canvas.setAttribute('height', `${roundedHeightWidth}px`)
-  const ctx = canvas.getContext("2d"),
-    boxSize = Math.floor(DIM / ROWS);
-  let frame = Array(ROWS * 3).fill().map(_ => new Table(ROWS))// TODO Rename Cell
+let frame = Array(ROW * 3).fill().map(_ => new Table(ROW))
+/* frame to modify when entering random numbers in cells
+so it is less iterations
+frame[0 ... ROW -1] - lines
+frame[ROW ... ROW * 2 -1] -columns
+frame[ROW * 2 ... ROW *3-1] - grid
+ */
 
-  let line = 0, row = 0, grid = 0;
-  const table = Array(ROWS * ROWS).fill().map((_, i) => {
-    line = Math.floor(i / ROWS) // SORT HORIZONTAL
-    row = i - ROWS * (line) //SORT VERTICAL
-    grid = (Math.floor(row / sqR) + Math.floor(line / sqR) * sqR)
-    return new Cell(frame[line].elmOptions, frame[row + ROWS].elmOptions, frame[grid + 2 * ROWS].elmOptions)
-  })
+/* Table is iteration through all Cells
+can use formulas of variables to reach quickly frames
+ */
 
+
+let line = 0, col = 0, grid = 0;
+const table = Array(ROW * ROW).fill().map((_, i) => {
+  line = Math.floor(i / ROW) // SORT HORIZONTAL
+  col = i - ROW * (line) //SORT VERTICAL
+  grid = Math.floor(col / sqR) + Math.floor(line / sqR) * sqR
+  return new Cell(frame[line].elmOptions, frame[col + ROW].elmOptions, frame[grid + 2 * ROW].elmOptions)
+})
+
+
+// frame[0].elmOptions.pop()
+// frame[0].elmOptions.pop()
+// // console.log(frame)
+// table[0].cellsOptions()
+
+drawBox(ctx, ROW, boxSize, DIM)
+
+
+let markedSqureX = 0
+let markedSqureY = 0
+let ableToWrite = false
+
+let tempX = 0, tempY = 0, tempGr = 0;
+
+const clearBox = (x, y, width = boxSize) => {
+  tempX = Math.floor(y / width)
+  tempY = Math.floor(x / width)
+  tempGr = Math.floor(tempY / sqR) + Math.floor(tempX / sqR) * sqR + 2 * ROW
+
+
+  console.log(tempX, tempY + ROW, tempGr, "clearBox")
+
+  x = Math.floor(x / width) * width;
+  y = Math.floor(y / width) * width;
+
+  ctx.clearRect(x, y, width, width)
+  markedSqureX = x;
+  markedSqureY = y;
+  ableToWrite = true
 }
-canvasScreen(4);
 
-/* Comented out for nice flow*/
+// Event Listeners
 
-//   function drawBox() {
-//     ctx.fillStyle = 'darkblue';
-//     ctx.fill();
-//     ctx.strokeStyle = 'black';
-//     let x = 0, y = 0;
+canvas.addEventListener('click', (e) => {
+  clearBox(e.offsetX, e.offsetY)
+})
 
-//     for (let row = 0; row <= ROWS; row++) {
-//       ctx.beginPath();
-
-//       // X axe
-//       x = row * boxSize;
-//       (row % sqR) === 0 ? ctx.lineWidth = 4 : ctx.lineWidth = 2;
-//       ctx.moveTo(x, y)
-//       ctx.lineTo(x, y + DIM)
-//       ctx.stroke();
-
-//       // Y axe
-//       ctx.moveTo(y, x)
-//       ctx.lineTo(y + DIM, x)
-//       ctx.stroke();
-//       ctx.closePath();
-//     }
-//   }
+document.addEventListener('keydown', (e) => {
+  let pressedKey = parseInt(e.key)
+  if (pressedKey && ableToWrite) {
+    ctx.font = `${boxSize / 2}px serif`;
+    ctx.fillText(`${pressedKey}`, markedSqureX + boxSize * .25, markedSqureY + boxSize * 0.75);
+    ableToWrite = false
+  }
+  drawBox(ctx, ROW, boxSize, DIM)
+})
 
 //   const syncTimeout = 200
 
@@ -76,16 +107,16 @@ canvasScreen(4);
 //   }
 
 //   const fillCells = () => {
-//     if (ROWS === 1) return;
+//     if (ROW === 1) return;
 //     let posX = 0;
 //     let posY = 0;
 //     let loopThroughTable = 0
 //     const outerGrid = async () => {
-//       while (loopThroughTable < ROWS) {
-//         await new Promise(r => setTimeout(r, syncTimeout * ROWS + syncTimeout));
+//       while (loopThroughTable < ROW) {
+//         await new Promise(r => setTimeout(r, syncTimeout * ROW + syncTimeout));
 //         slowFilling(table[loopThroughTable].options, posX, posY)
 //         posX += (sqR * boxSize)
-//         if (posX === boxSize * ROWS) {
+//         if (posX === boxSize * ROW) {
 //           posX = 0;
 //           posY += boxSize * sqR;
 //         }
@@ -94,16 +125,3 @@ canvasScreen(4);
 //     }
 //     outerGrid()
 //   }
-//   drawBox()
-//   fillCells();
-// }
-
-// canvasScreen();
-
-// const fillButton = document.getElementById('fillButton');
-// const choiceNumber = document.getElementById('sqNumber')
-
-// fillButton.addEventListener('click', () => {
-//   fillButton.disabled = true;
-//   canvasScreen(parseInt(choiceNumber.value, 10))
-// });
